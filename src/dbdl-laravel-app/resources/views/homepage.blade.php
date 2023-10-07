68ffedc3b8d04097124e936461f7eba3728bb792
Caleb Harris <charris4@uccs.edu>
2023-10-07 12:12:55 -0600

    Added SASS Support, started creating homepage

---
+++ src/dbdl-laravel-app/resources/views/homepage.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DBDL Perk Randomizer</title>
+    @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="navbar-container">
+        <nav class="navbar">
+            <div class="navbar-left">
+                <ul>
+                    <li><a href="#" class="active">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+            <div class="navbar-right">
+                <ul>
+                    <li><a href="#">Settings</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</body>
+</html>
